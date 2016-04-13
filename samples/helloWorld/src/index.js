a47931681051170868c6fe8b03fe843947950cf9
Keefr <clearbusiness@gmail.com>
2016-04-13 10:47:26 -0400

    Update index.js

--- samples/helloWorld/src/index.js
+++ samples/helloWorld/src/index.js
@@ -21,7 +21,7 @@
 /**
  * App ID for the skill
  */
-var APP_ID = undefined; //replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
+var APP_ID = undefined; //replace with "amzn1.echo-sdk-ams.app.[amzn1.echo-sdk-ams.app.ecee2a67-e6a4-489c-905d-bbb5104e9c86]";
 
 /**
  * The AlexaSkill prototype and helper functions
